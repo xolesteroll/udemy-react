dcd37f523683f9a48908b96b10b679a44fc1e436
Anton <Iarovoi>
2021-11-09 16:44:12 +0200

    added filter by year component and some other stufff

---
+++ first/src/components/ExpensesFilter/ExpensesFilter.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import styles from './ExpensesFilter.module.css';
+
+const ExpensesFilter = (props) => {
+    return (
+        <div className={styles.expensesFilter}>
+            <div className={styles.expensesFilterControl}>
+                <label>Filter by year</label>
+                <select value={props.year} onChange={props}>
+                    <option value='2022'>2022</option>
+                    <option value='2021'>2021</option>
+                    <option value='2020'>2020</option>
+                    <option value='2019'>2019</option>
+                </select>
+            </div>
+        </div>
+    );
+};
+
+export default ExpensesFilter;
